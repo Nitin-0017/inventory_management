f7df64bfbc3f117a52704a07316a665b698eb1e2
Nitin-0017 <raonitinkumar06@gmail.com>
2025-08-30 12:12:28 +0530

    Final commit

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './SignUp.css';
+import './Signup.css';
 
 const SignUp = () => {
   const [fullName, setFullName] = useState('');
